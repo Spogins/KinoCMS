52f06512abf4f913b4d0593ee86b024b88af3210
Bogdan <spogins@gmail.com>
2023-01-10 13:58:53 +0200

    init

--- kinocms/settings.py
+++ kinocms/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
     'modeltranslation',
     'users.apps.UsersConfig',
     # General use templates & template tags (should appear first)
-    'adminlte3',
-    # Optional: Django admin theme (must be before django.contrib.admin)
-    'adminlte3_theme',
+    # 'adminlte3',
+    # # Optional: Django admin theme (must be before django.contrib.admin)
+    # 'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
